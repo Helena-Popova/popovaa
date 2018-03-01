f5e9b1c1082e2723a53532f8f24fe1531f4cb21b
Helena-Popova <helezpopova@gmail.com>
2018-03-01 21:59:35 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j;
 
+/**
+ *Class Calculate решение задачи части 001 урок 1.
+ *@author Popova
+ *@since 01.03.2018
+*/
 public class Calculate {
-
+	/**
+	* Конструктор вывода строки на консоль.
+	* @param args - args.
+	*/
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
