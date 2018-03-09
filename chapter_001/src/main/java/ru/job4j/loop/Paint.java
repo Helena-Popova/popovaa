741295974e66f3648155677bed9b6bd9d9d5ac2b
Helena-Popova <helezpopova@gmail.com>
2018-03-09 16:43:18 +0300

    4.5. Рефакторинг кода.

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,5 +1,7 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * Class for drawing pyramids
  * @author Helena
@@ -16,19 +18,7 @@ public class Paint {
      */
 
     public String pyramid(int height) {
-        StringBuilder screen = new StringBuilder();
-        int weight = 2 * height - 1;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != weight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(height,2 * height - 1,(row,column)-> row >= height - column - 1 && row + height - 1 >= column);
     }
     /**
      * method for drawing pyramids
@@ -36,23 +26,23 @@ public class Paint {
      * @return pyramid
      */
     public String simplePiramid(int height){
+        int width = height>0 ? (2*height -1) : 0;
+        return this.loopBy(height,width,(row,column)-> column >= (width/2 - row) && column <= (width/2 + row));
+    }
+
+    private String loopBy(int height, int weight,BiPredicate<Integer, Integer> predict){
         StringBuilder screen = new StringBuilder();
         String lineNew = System.lineSeparator();
-
-        int width = height>0 ? (2*height -1) : 0;
-
-        for(int row = 0; row< height; row++){
-            for(int column = 0; column< width; column++){
-                if ( column >= (width/2 - row) && column <= (width/2 + row)) {
+        for(int row = 0; row < height ; row++) {
+            for (int column = 0; column < weight; column++) {
+                if (predict.test(row, column)) {
                     screen.append("^");
-                }
-                else{
+                } else {
                     screen.append(" ");
+                    }
                 }
+                screen.append(lineNew);
             }
-            screen.append(lineNew);
-        }
-        return screen.toString();
-
+            return screen.toString();
     }
 }
