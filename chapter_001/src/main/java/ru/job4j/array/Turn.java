e9be37b21136d8ee9074d612aecb45a1e990b576
Helena-Popova <helezpopova@gmail.com>
2018-03-10 13:38:37 +0300

    5.2. Перевернуть массив.

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+/**
+ * @author Helena
+ * @version 1.0
+ * @since 10.03.2018
+*/
+public class Turn {
+
+	/**
+	 * method for turn back of array
+	 * @param array
+	 * @return array which turned
+	 */
+	public int[] back(int[] array) {
+		
+		for(int index = 0; index < array.length/2; index++){
+			int temp = array[index];
+			array[index] = array[array.length - index -1];
+			array[array.length - index -1] = temp;
+		}
+		return array;
+	}
+
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+
+import static org.junit.Assert.*;
+
+public class TurnTest {
+
+    @Test
+    public void whenTurnArrayWithAnEvenlength(){
+        int[] myEvenArray = {1,2,3,4};
+        Turn someAlgoritmTurn = new Turn();
+        int[] result = someAlgoritmTurn.back(myEvenArray);
+        assertThat(result, is(new int[]{4,3,2,1}));
+    }
+
+    @Test
+    public void whenTurnArrayWithAnOddlength(){
+        int[] myOddArray = {1,2,3,4,5};
+        Turn someAlgoritmTurn = new Turn();
+        int[] result = someAlgoritmTurn.back(myOddArray);
+        assertThat(result, is(new int[]{5,4,3,2,1}));
+    }
+}
