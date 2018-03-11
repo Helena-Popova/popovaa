3fbe325a91dca0287b68c1531bf133994104a65a
Helena-Popova <helezpopova@gmail.com>
2018-03-11 17:16:54 +0300

    5.3. Создать программу для сортировки массива методом перестановки.

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -17,10 +17,10 @@ public class BubbleSort {
 	public int[] sort(int[] array) {
 		
 		int numberOfPasses = array.length;
-		while(numberOfPasses > 1){
-			for(int index = 0; index < numberOfPasses-1; index++){
-				if(array[index] > array[index+1]){
-				int temp = array[index+1];
+		while (numberOfPasses > 1) {
+			for (int index = 0; index < numberOfPasses - 1; index++) {
+				if (array[index] > array[index + 1]) {
+				int temp = array[index + 1];
 				array[index + 1] = array[index];
 				array[index] = temp;
 				}
--- chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -9,10 +9,10 @@ public class BubbleSortTest {
 
     @Test
     public void whenSortMassiv() {
-        int[] myArrayForSort = {9,8,13,6,5,4,0,2,1};
+        int[] myArrayForSort = {9, 8, 13, 6, 5, 4, 0, 2, 1};
         BubbleSort someSort = new BubbleSort();
         int[] result = someSort.sort(myArrayForSort);
-        assertThat(result, is(new int[] {0,1,2,4,5,6,8,9,13}));
+        assertThat(result, is(new int[] {0, 1, 2, 4, 5, 6, 8, 9, 13}));
     }
 
 }
