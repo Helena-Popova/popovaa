4d4fd16baa7b93f5f4c53dad687d9cfae25ddfe2
Helena-Popova <helezpopova@gmail.com>
2018-03-11 15:03:06 +0300

    5.6. Удаление дубликатов в массиве.

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,34 @@
+package ru.job4j.array;
+ import java.util.Arrays;
+
+/**
+ * @author Helena
+ * @version 1.0
+ * @since 10.03.2018
+*/
+
+public class ArrayDuplicate {
+	/**
+	 * metod for remove dublicate
+	 * @param array
+	 * @return array with deleted duplicates
+	 */
+	public String[] remove(String[] array) {
+		
+		int repeatCounter = array.length-1;
+			for(int index = 0; index < array.length; index++){
+				String temp = array[index];
+				for (int indexCompare = index+1; indexCompare < repeatCounter;){
+					if (temp.equals(array[indexCompare])){
+						array[indexCompare] = array[repeatCounter];
+						repeatCounter --;
+					}
+					else {
+						indexCompare++;
+					}
+					
+				}
+			}
+			return Arrays.copyOf(array,repeatCounter);
+	}
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ArrayDuplicateTest {
+
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate(){
+        String[] myFullArray = {"1","2","1","3","2","5","5"};
+        ArrayDuplicate duplicateArray = new ArrayDuplicate();
+        String[]  result = duplicateArray.remove(myFullArray);
+        assertThat(result, is(new String[]{"1","2","5","3"}));
+    }
+
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicatewithOneNumbers(){
+        String[] myFullArray = {"1","1","1","1","1","1","1"};
+        ArrayDuplicate duplicateArray = new ArrayDuplicate();
+        String[]  result = duplicateArray.remove(myFullArray);
+        assertThat(result, is(new String[]{"1"}));
+    }
+
+}
