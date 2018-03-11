215c0afb69aaf300ba4731e003b01411c26bfd43
Helena-Popova <helezpopova@gmail.com>
2018-03-11 17:11:15 +0300

    5.1. Классический поиск перебором.

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -16,8 +16,8 @@ public class FindLoop {
 	 */
 	public int indexOf(int[] data, int element) {
 		int result = -1;
-		for(int index = 0; index < data.length ; index++){
-			if(data[index] == element){
+		for (int index = 0; index < data.length; index++) {
+			if (data[index] == element) {
 				result = index;
 				break;
 			}
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -8,27 +8,27 @@ import static org.junit.Assert.*;
 public class FindLoopTest {
 
     @Test
-    public void whenFindRightIndexFrom10Numbers(){
+    public void whenFindRightIndexFrom10Numbers() {
 
-        int[] myMassif = {1,2,3,4,5,6,7,8,9,10};
+        int[] myMassif = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         FindLoop someFindAlgotitm = new FindLoop();
-        int result = someFindAlgotitm.indexOf(myMassif,3);
+        int result = someFindAlgotitm.indexOf(myMassif, 3);
         assertThat(result, is(2));
     }
 
     @Test
-    public void whenFindNonexistentElement(){
-        int[] myMassif = {33,23,24,45,56,46};
+    public void whenFindNonexistentElement() {
+        int[] myMassif = {33, 23, 24, 45, 56, 46};
         FindLoop someFindAlgotitm = new FindLoop();
-        int result = someFindAlgotitm.indexOf(myMassif,32);
+        int result = someFindAlgotitm.indexOf(myMassif, 32);
         assertThat(result, is(-1));
     }
 
     @Test
-    public void whenFindNullButIndexWrong(){
-        int[] myMassif = {3,23,24,45,56,46};
+    public void whenFindNullButIndexWrong() {
+        int[] myMassif = {3, 23, 24, 45, 56, 46};
         FindLoop someFindAlgotitm = new FindLoop();
-        int result = someFindAlgotitm.indexOf(myMassif,0);
+        int result = someFindAlgotitm.indexOf(myMassif, 0);
         assertThat(result, is(0));
     }
 
