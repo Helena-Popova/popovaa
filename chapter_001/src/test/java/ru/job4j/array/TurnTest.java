81438274cf122aeba850db63de95af08f3640e82
Helena-Popova <helezpopova@gmail.com>
2018-03-11 17:13:55 +0300

    5.2. Перевернуть массив.

--- chapter_001/src/test/java/ru/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -8,18 +8,18 @@ import static org.junit.Assert.*;
 public class TurnTest {
 
     @Test
-    public void whenTurnArrayWithAnEvenlength(){
-        int[] myEvenArray = {1,2,3,4};
+    public void whenTurnArrayWithAnEvenlength() {
+        int[] myEvenArray = {1, 2, 3, 4};
         Turn someAlgoritmTurn = new Turn();
         int[] result = someAlgoritmTurn.back(myEvenArray);
-        assertThat(result, is(new int[]{4,3,2,1}));
+        assertThat(result, is(new int[]{4, 3, 2, 1}));
     }
 
     @Test
-    public void whenTurnArrayWithAnOddlength(){
-        int[] myOddArray = {1,2,3,4,5};
+    public void whenTurnArrayWithAnOddlength() {
+        int[] myOddArray = {1, 2, 3, 4, 5};
         Turn someAlgoritmTurn = new Turn();
         int[] result = someAlgoritmTurn.back(myOddArray);
-        assertThat(result, is(new int[]{5,4,3,2,1}));
+        assertThat(result, is(new int[]{5, 4, 3, 2, 1}));
     }
 }
