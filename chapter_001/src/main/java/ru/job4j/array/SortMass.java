4fcc1a94945ecc5dca355ab5fffc0b135fbfc339
Helena-Popova <helezpopova@gmail.com>
2018-03-16 07:09:39 +0300

    Слияние двух отсортированных массивов в третий отсортированный

--- chapter_001/src/main/java/ru/job4j/array/SortMass.java
+++ chapter_001/src/main/java/ru/job4j/array/SortMass.java
@@ -1,4 +1,30 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
+/**
+ * @author Helena
+ * @version 1.0
+ * @since 15.03.18
+ */
+
 public class SortMass {
+
+    public int[] sort(int[] massFirst, int[] massSecond) {
+
+        int[] massSort = new int[massFirst.length + massSecond.length];
+        int a = 0;
+        int i = 0;
+
+        for ( int counter = 0; counter < massSort.length; counter++){
+            if (i==massSecond.length || a < massFirst.length && massFirst[a] < massSecond[i] ) {
+                massSort[counter] = massFirst[a++];
+            }
+            else {
+                massSort[counter] = massSecond[i++];
+            }
+        }
+        return massSort;
+    }
 }
+
--- chapter_001/src/test/java/ru/job4j/array/SortMassTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SortMassTest.java
@@ -1,5 +1,20 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class SortMassTest {
+
+   @Test
+    public void whenWeHaveCoupleSortMassThenReturnSortMultiplateMass() {
+        int[] first = new int[]{1,6,7};
+        int[] second = new int[]{3,5,9,10};
+        SortMass massive = new SortMass();
+        int[] result = massive.sort(first,second);
+        assertThat(result, is(new int[]{1,3,5,6,7,9,10}));
+
+    }
 
 }
