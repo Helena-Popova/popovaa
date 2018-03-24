f95bf8c09914df3015a0223f8fad766e9e9a09c7
Helena-Popova <helezpopova@gmail.com>
2018-03-24 15:53:30 +0300

    class Tracker: изменён метод нахождения по имени, если метод ничего не нашел,то возвращает массив с первым эементов нулевым

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -16,16 +16,26 @@ public class Tracker {
 	static String generateId() {
 		return String.valueOf(System.currentTimeMillis() + RM.nextInt());
 	}
-	
-	
+
+	/**
+	 * Метод заменяте отдно значение на другое.
+	 * @since 24.03.18 // обавлена строка,чтобы id не изменялись.
+	 * @param id
+	 * @param item
+	 */
 	public void replace(String id, Item item) {
 		for (int i = 0; i < count; i++) {
 			if (this.items[i].getId().equals(id)) {
 				this.items[i] = item;
+				this.items[i].setId(id);
 			}
 		}
 	}
-	
+
+	/**
+	 * Метод удаляет найденное по id значение из списка.
+	 * @param id
+	 */
 	public void delete(String id) {
 		for (int i = 0; i < count; i++) {
 			if (this.items[i].getId().equals(id)) {
@@ -36,7 +46,11 @@ public class Tracker {
 			}
 		}
 	}
-	
+
+	/**
+	 * Возвращает все ненулевые элементы списка.
+	 * @return список Item[]
+	 */
 	public Item[] findAll() {
 		Item[] itemsCopy = Arrays.copyOf(this.items, count);
 		int index = count;
@@ -48,16 +62,23 @@ public class Tracker {
 		}
 		return Arrays.copyOf(itemsCopy, cCopy);
 	}
-	
+
+	/**
+	 * Находит все эелементы в списке по ключю, вовращает массивом.
+	 * @since 24.03.18 довалено условие,что если ничего не найденно,то возвращает нулевой элемент.
+	 * @param key
+	 * @return найденные занчения.
+	 */
 	public Item[] findByName(String key) {
 		Item[] itemsCopy = new Item[100];
+		itemsCopy[0]  = Item.EMPTY;
 		int cCopy = 0;
 		for (int i = 0; i < count; i++) {
-			if (this.items[i].getName().equals(key)) {
+			if(this.items[i].getName(). equals(key)){
 				System.arraycopy(this.items, i, itemsCopy, cCopy++, 1);
 			}
 		}
-		return Arrays.copyOf(itemsCopy, cCopy);
+		return cCopy > 0 ? Arrays.copyOf(itemsCopy,cCopy) : Arrays.copyOf(itemsCopy,1);
 	}
 	
 	protected Item findById(String id) {
