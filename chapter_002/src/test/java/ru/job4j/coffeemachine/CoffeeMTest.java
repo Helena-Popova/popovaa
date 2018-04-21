5fb8f3b0972964d08e37b940d176e751eb5b613f
Helena-Popova <helezpopova@gmail.com>
2018-04-21 19:56:53 +0300

    Тесты к кофемашине

--- chapter_002/src/test/java/ru/job4j/coffeemachine/CoffeeMTest.java
+++ chapter_002/src/test/java/ru/job4j/coffeemachine/CoffeeMTest.java
@@ -1,5 +1,39 @@
+package ru.job4j.coffeemachine;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import ru.job4j.coffeemachine.exceptions.*;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class CoffeeMTest {
+
+    CoffeeM coffeM = new CoffeeM();
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void valueIsNegative() throws ValueError {
+        exception.expect(ValueError.class);
+        exception.expectMessage("Недостаточно денег для выдачи кофе");
+        coffeM.changes(5, 35);
+
+    }
+
+    @Test
+    public void priceIsNegative() throws ValueError {
+        exception.expect(ValueError.class);
+        exception.expectMessage("Неверно указана цена товара");
+        coffeM.changes(5, -4);
+    }
+
+    @Test
+    public void changesTake() {
+        int[] result = coffeM.changes(47, 35);
+        assertThat(result, is(new int[] {10, 2}));
+    }
 
 }
