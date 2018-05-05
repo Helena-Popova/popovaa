03805ae02afbfa3641887384194e1b8c5afe16f7
Helena-Popova <helezpopova@gmail.com>
2018-05-05 15:32:17 +0300

    Очередь с приоритетом

--- chapter_003/src/main/java/queue/PriorityQueue.java
+++ chapter_003/src/main/java/queue/PriorityQueue.java
@@ -1,4 +1,25 @@
 package queue;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    public void put(Task task) {
+        ListIterator<Task> iter = tasks.listIterator();
+        int index = 0;
+        while (iter.hasNext() && iter.next().getPriority() < task.getPriority()) {
+            index = iter.nextIndex();
+        }
+        tasks.add(index, task);
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+
+    public LinkedList<Task> getTasks() {
+        return tasks;
+    }
 }
--- chapter_003/src/main/java/queue/Task.java
+++ chapter_003/src/main/java/queue/Task.java
@@ -1,4 +1,40 @@
 package queue;
 
+/**
+ * Задача с приоритетом
+ *@author Helena
+ *@version 1.0
+ *@since 06.03.18
+ */
 public class Task {
+    private String desc;
+    private int priority;
+
+    /**
+     * @param desc описание
+     * @param priority приоритет
+     */
+    public Task(String desc, int priority) {
+        this.desc = desc;
+        this.priority = priority;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{"
+                + "desc='"
+                + desc
+                + '\''
+                + ", priority="
+                + priority
+                + '}';
+    }
 }
