c5b469661c03d44fd1b293f23dea6c372bfd913f
Helena-Popova <helezpopova@gmail.com>
2018-05-05 21:24:51 +0300

    Конвертация двумерного массива в ArrayLis

---
+++ chapter_003/src/main/java/convert/ConvertArray2List.java
@@ -0,0 +1,27 @@
+package convert;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Конвертация двумерного массива в ArrayList
+ * @author Helena
+ */
+public class ConvertArray2List {
+
+    /**
+     * Конвертация двумерного массива в ArrayList
+     * @param array входящий массив для конвертации
+     * @return список со значениями из массива
+     */
+    public List<Integer> toList(int[][] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int[] mass : array) {
+            list.addAll(Arrays.stream(mass).boxed().collect(Collectors.toList()));
+        }
+        return list;
+    }
+}
---
+++ chapter_003/src/test/java/convert/ConvertArray2ListTest.java
@@ -0,0 +1,34 @@
+package convert;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ConvertArray2ListTest {
+    @Test
+    public void when2on2ArrayThenList4() {
+        ConvertArray2List list = new ConvertArray2List();
+        int[][] input = {
+                {1, 2},
+                {3, 4}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayIsEmptyThenListIsEmpty() {
+        ConvertArray2List list = new ConvertArray2List();
+        int[][] input = {};
+        List<Integer> expect = Arrays.asList();
+        List<Integer> result = list.toList(input);
+        assertThat(expect.isEmpty(), is(result.isEmpty()));
+    }
+}
