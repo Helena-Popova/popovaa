e78d902fce0e4c576b48b2f1e04f79483fb31930
Helena-Popova <helezpopova@gmail.com>
2018-05-10 07:18:30 +0300

    1. Организовать сортировку User [#10034]

--- chapter_003/src/main/java/sort/SortUser.java
+++ chapter_003/src/main/java/sort/SortUser.java
@@ -1,4 +1,29 @@
 package sort;
 
-public class SortUsers {
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ *
+ Необходимо создать модель User с полями name, age.
+ Класс User должен реализовать интерфейс Comparable.
+ В классе SortUser написать метод public Set<User> sort (List<User>),
+ который будет возвращать TreeSet пользователей,
+ отсортированных по возрасту в порядке возрастания.
+ */
+public class SortUser {
+
+    public Set<User> sort(List<User> userList) {
+        Set<User> result = new TreeSet<>();
+        userList.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        result.addAll(userList);
+        return result;
+    }
 }
--- chapter_003/src/test/java/sort/SortUserTest.java
+++ chapter_003/src/test/java/sort/SortUserTest.java
@@ -1,5 +1,27 @@
+package sort;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class SortUserTest {
+
+    @Test
+    public void whenSortSomeUsersListByAge() {
+        SortUser sortUser = new SortUser();
+        List<User> users = new ArrayList<>();
+        users.add(new User("Gulchitay", 23));
+        users.add(new User("Petya", 3));
+        users.add(new User("Masha", 14));
+        Set<User> result =  sortUser.sort(users);
+        assertThat(result.iterator().next(), is(new User("Petya", 3)));
+
+    }
 
 }
