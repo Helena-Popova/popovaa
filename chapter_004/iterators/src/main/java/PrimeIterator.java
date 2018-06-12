3ccd1a7d37988cac3adb7a9327933609d6b60b91
Helena-Popova <helezpopova@gmail.com>
2018-06-12 18:22:18 +0300

    итератор простых чисел'

--- chapter_004/iterators/src/main/java/PrimeIterator.java
+++ chapter_004/iterators/src/main/java/PrimeIterator.java
@@ -1,4 +1,58 @@
-package PACKAGE_NAME;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class PrimeIterator {
+public class PrimeIterator implements Iterator {
+
+    private int[] numbers;
+    private int index = 0;
+
+    public PrimeIterator(final int[] aNumbers) {
+        numbers = aNumbers;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return findElement() != null;
+    }
+
+    @Override
+    public Object next() {
+        if (findElement() == null) {
+            throw new NoSuchElementException();
+        }
+        index += findElement();
+        Integer count = numbers[index];
+        index++;
+        return count;
+    }
+
+    boolean isItPrimeNumber(int i) {
+        boolean result = true;
+        int count = 2;
+        if (i == 1) {
+            result = false;
+        } else {
+            while (count < i ) {
+                if (i%count == 0 ) {
+                    result =  false;
+                    break;
+                }
+                count++;
+            }
+        }
+        return result;
+    }
+
+    private Integer findElement() {
+        Integer result  = null;
+        int count = 0;
+        while (index + count < numbers.length) {
+            if (isItPrimeNumber(numbers[index +count])) {
+                result = count;
+                break;
+            }
+            count++;
+        }
+        return result;
+    }
 }
