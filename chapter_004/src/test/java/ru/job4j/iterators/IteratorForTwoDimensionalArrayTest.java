58e7770735e771b653b674c0f2c072db68203bd5
Helena-Popova <helezpopova@gmail.com>
2018-06-12 17:07:57 +0300

    delete

--- chapter_004/src/test/java/ru/job4j/iterators/IteratorForTwoDimensionalArrayTest.java
+++ chapter_004/src/test/java/ru/job4j/iterators/IteratorForTwoDimensionalArrayTest.java
@@ -10,55 +10,5 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class IteratorForTwoDimensionalArrayTest {
-    private Iterator<Integer> it;
 
-    @Before
-    public void setUp() {
-        it = new IteratorForTwoDimensionalArray(new int[][]{{1, 2, 3}, {4, 5, 6}});
-    }
-
-    @Test
-    public void hasNextNextSequentialInvocation() {
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(1));
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(2));
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(3));
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(4));
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(5));
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(6));
-        assertThat(it.hasNext(), is(false));
-    }
-
-    @Test
-    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation() {
-        assertThat(it.next(), is(1));
-        assertThat(it.next(), is(2));
-        assertThat(it.next(), is(3));
-        assertThat(it.next(), is(4));
-        assertThat(it.next(), is(5));
-        assertThat(it.next(), is(6));
-    }
-
-    @Test
-    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(1));
-        assertThat(it.next(), is(2));
-        assertThat(it.next(), is(3));
-        assertThat(it.next(), is(4));
-        assertThat(it.next(), is(5));
-        assertThat(it.next(), is(6));
-    }
-
-    @Test(expected = NoSuchElementException.class)
-    public void shoulThrowNoSuchElementException() {
-        it = new IteratorForTwoDimensionalArray(new int[][]{});
-        it.next();
-    }
 }
