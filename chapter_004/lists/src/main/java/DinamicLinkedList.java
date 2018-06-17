03b454e2efa04e6898f8880bfd59e2750f85e505
Helena-Popova <helezpopova@gmail.com>
2018-06-17 23:28:33 +0300

    5.3.2. Создать контейнер на базе связанного списка  [#159]

--- chapter_004/lists/src/main/java/DinamicLinkedList.java
+++ chapter_004/lists/src/main/java/DinamicLinkedList.java
@@ -1,4 +1,116 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class DinamicLinkedList {
+public class DinamicLinkedList<E> implements Iterable<E> {
+
+    private Node<E> first;
+    int modCount = 0;
+    int size = 0;
+
+    private static class Node<E> {
+        E date = null;
+        Node<E> next = null;
+        Node<E> previous = null;
+
+        Node(E date) {
+            this.date = date;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{"
+                    + "date=" + date.toString()
+                    + '}';
+        }
+    }
+
+    public void add(E value) {
+        Node<E> newLink = new Node<E>(value);
+        if (this.first == null) {
+            this.first = newLink;
+            this.first.next = null;
+        } else {
+            this.first.previous = newLink;
+            newLink.next = first;
+            this.first = newLink;
+        }
+        size++;
+    }
+
+    public E get(int index) {
+        if (index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> result = this.first;
+        int i = 0;
+        while (i < index) {
+            result = result.next;
+            i++;
+        }
+        return result.date;
+    }
+
+    public Iterator<E> iterator() {
+        Iterator<E> iterator = new Iterator<E>() {
+
+            private Node<E> iteratorStep = first;
+            private Node<E> previousStep = null;
+            int expectedModCount = modCount;
+
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return iteratorStep != null;
+            }
+
+            public E next() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+
+                previousStep = iteratorStep;
+                iteratorStep = iteratorStep.next;
+                return previousStep.date;
+            }
+
+            public void remove() {
+                if (previousStep.previous != null) {
+                    previousStep.previous.next = previousStep.next;
+                } else {
+                    previousStep.next.previous = null;
+                    first = previousStep.next;
+                }
+                if (previousStep.next != null) {
+                    previousStep.next.previous = previousStep.previous;
+                } else {
+                    previousStep.previous.next = null;
+                }
+                modCount++;
+                expectedModCount++;
+                size--;
+            }
+
+        };
+        return iterator;
+    }
+
+
+    public boolean hasCycle() {
+        boolean result = false;
+        Iterator<E> iterator = iterator();
+        Iterator<E> iterator1 = iterator();
+        while (iterator.hasNext()) {
+            E step = iterator.next();
+            while (iterator1.hasNext()) {
+                if (iterator.next() == iterator1.next()) {
+                    System.out.println("log");
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
 }
--- chapter_004/lists/src/test/java/DinamicLinkedListTest.java
+++ chapter_004/lists/src/test/java/DinamicLinkedListTest.java
@@ -1,5 +1,54 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class DinamicLinkedListTest {
+    DinamicLinkedList<Integer> dinamicLinkedList = new DinamicLinkedList();
 
+    @Before
+    public void setUp() {
+        int i = 99;
+        while (i >= 0) {
+            dinamicLinkedList.add(i);
+            i--;
+        }
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetOneResultTwo() {
+        assertThat(dinamicLinkedList.get(0), is(0));
+        assertThat(dinamicLinkedList.get(77), is(77));
+        assertThat(dinamicLinkedList.get(99), is(99));
+    }
+
+    @Test
+    public void whenDeleteElementsThanGetNewFirstElement() {
+        Iterator<Integer> integerIterator = dinamicLinkedList.iterator();
+        assertThat(integerIterator.hasNext(), is(true));
+        assertThat(integerIterator.hasNext(), is(true));
+        integerIterator.next();
+        integerIterator.next();
+        integerIterator.next();
+        integerIterator.remove();
+        Iterator<Integer> integerIterator1 = dinamicLinkedList.iterator();
+        integerIterator1.next();
+        integerIterator1.next();
+        integerIterator1.next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void shouldReturnEvenNumbersSequentially() {
+        Iterator<Integer> integerIterator = dinamicLinkedList.iterator();
+        Iterator<Integer> integerIterator1 = dinamicLinkedList.iterator();
+        assertTrue(integerIterator.hasNext());
+        assertTrue(integerIterator1.hasNext());
+        integerIterator.remove();
+        integerIterator1.hasNext();
+
+    }
 }
