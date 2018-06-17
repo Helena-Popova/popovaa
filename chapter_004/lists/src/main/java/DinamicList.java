ffc0ba0410d69a62022dafd88ee93cbd88798186
Helena-Popova <helezpopova@gmail.com>
2018-06-17 23:27:52 +0300

    5.3.1. Создать динамический список на базе массива. [#158]

--- chapter_004/lists/src/main/java/DinamicList.java
+++ chapter_004/lists/src/main/java/DinamicList.java
@@ -1,4 +1,72 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class DinamicList {
+public class DinamicList<E> implements Iterable<E> {
+    private Object[] container = new Object[10];
+    int index  = 0;
+    int modCount = 0;
+
+    public boolean add(E value) {
+        if (container.length != 0 && container.length % 10 == 0) {
+            container = Arrays.copyOf(container, container.length + 10);
+        }
+        container[index++] = value;
+        modCount++;
+        return true;
+    }
+
+    public E get(int index) {
+        return (E) container[index];
+    }
+
+    public Iterator<E> iterator() {
+        Iterator<E> iterator = new Iterator<E>() {
+
+            int expectedModCount = modCount;
+            private int count = 0;
+
+
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                boolean result = false;
+                int i = count;
+                if (count < container.length) {
+                    while (i < container.length) {
+                        if (container[count] != null) {
+                            result = true;
+                            break;
+                        }
+                        i++;
+                    }
+                }
+                return result;
+            }
+
+            public E next() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (count >= container.length) {
+                    throw new NoSuchElementException();
+                }
+                return (E) container[count++];
+            }
+
+            public void remove() {
+                if (count > container.length || count < 0) {
+                    throw new IndexOutOfBoundsException();
+                }
+                container[count] = null;
+                count--;
+                expectedModCount++;
+                modCount++;
+                index--;
+            }
+        };
+        return iterator;
+    }
 }
--- chapter_004/lists/src/test/java/DinamicListTest.java
+++ chapter_004/lists/src/test/java/DinamicListTest.java
@@ -1,5 +1,48 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class DinamicListTest {
+    DinamicList<Integer> dinamicList = new DinamicList();
 
+    @Before
+    public void setUp() {
+        int i = 0;
+        while (i < 10) {
+            dinamicList.add(i);
+            i++;
+        }
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetOneResultTwo() {
+        assertThat(dinamicList.get(0), is(0));
+        assertThat(dinamicList.get(5), is(5));
+        assertThat(dinamicList.get(9), is(9));
+    }
+
+    @Test
+    public void whenDeleteElementsThanGetNewFirstElement() {
+        Iterator<Integer> integerIterator = dinamicList.iterator();
+        assertThat(integerIterator.hasNext(), is(true));
+        assertThat(integerIterator.hasNext(), is(true));
+        assertThat(0, is(dinamicList.get(0)));
+        assertThat(1, is(dinamicList.get(1)));
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void shouldReturnEvenNumbersSequentially() {
+        Iterator<Integer> integerIterator = dinamicList.iterator();
+        Iterator<Integer> integerIterator1 = dinamicList.iterator();
+        assertTrue(integerIterator.hasNext());
+        assertTrue(integerIterator1.hasNext());
+        integerIterator.remove();
+        integerIterator1.hasNext();
+
+    }
 }
