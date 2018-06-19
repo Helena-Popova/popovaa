8cb88c128888afe8afbb7b4a1f8ec374aeeb27fb
Helena-Popova <helezpopova@gmail.com>
2018-06-20 00:27:41 +0300

    stack

--- chapter_004/lists/src/main/java/containers/SimpleStack.java
+++ chapter_004/lists/src/main/java/containers/SimpleStack.java
@@ -1,4 +1,25 @@
 package containers;
+import java.util.NoSuchElementException;
 
-public class SimpleStack {
+import lists.DinamicLinkedList;
+
+import java.util.Iterator;
+
+public class SimpleStack<T> {
+    DinamicLinkedList<T> linkedList = new DinamicLinkedList<>();
+
+
+    public T poll() {
+        Iterator<T> iterator = linkedList.iterator();
+        if (!iterator.hasNext()) {
+            throw new NoSuchElementException();
+        }
+        T result = iterator.next();
+        iterator.remove();
+        return result;
+    }
+
+    public void push(T value) {
+        linkedList.add(value);
+    }
 }
--- chapter_004/lists/src/test/java/containers/SimpleStackTest.java
+++ chapter_004/lists/src/test/java/containers/SimpleStackTest.java
@@ -1,5 +1,37 @@
+package containers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class SimpleStackTest {
+    SimpleStack<Integer> stack = new SimpleStack();
 
+    @Before
+    public void setUp() {
+        int i = 0;
+        while (i < 10) {
+            stack.push(i);
+            i++;
+        }
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void poll() {
+        assertThat(9, is(stack.poll()));
+        assertThat(8, is(stack.poll()));
+        assertThat(7, is(stack.poll()));
+        assertThat(6, is(stack.poll()));
+        assertThat(5, is(stack.poll()));
+        assertThat(4, is(stack.poll()));
+        assertThat(3, is(stack.poll()));
+        assertThat(2, is(stack.poll()));
+        assertThat(1, is(stack.poll()));
+        assertThat(0, is(stack.poll()));
+        stack.poll();
+    }
 }
