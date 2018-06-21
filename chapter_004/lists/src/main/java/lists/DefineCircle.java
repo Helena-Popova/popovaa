2caa71697072b50e2fa1e8a5941a0cbb0b4d2b57
Helena-Popova <helezpopova@gmail.com>
2018-06-21 23:08:59 +0300

    define circle in some node linked list

--- chapter_004/lists/src/main/java/lists/DefineCircle.java
+++ chapter_004/lists/src/main/java/lists/DefineCircle.java
@@ -1,4 +1,23 @@
 package lists;
 
+import java.util.Iterator;
+
 public class DefineCircle {
+    public boolean hasCycle(DinamicLinkedList.Node first) {
+        boolean result = false;
+        DinamicLinkedList.Node storeV = first;
+        DinamicLinkedList.Node circleV = first;
+        LabelRound: while (storeV != null) {
+            while (circleV.next != storeV.next) {
+                if (storeV.next == circleV) {
+                    result = true;
+                    break LabelRound;
+                }
+                circleV = circleV.next;
+            }
+            storeV = storeV.next;
+            circleV = first;
+        }
+        return result;
+    }
 }
--- chapter_004/lists/src/test/java/lists/DefineCircleTest.java
+++ chapter_004/lists/src/test/java/lists/DefineCircleTest.java
@@ -1,5 +1,32 @@
+package lists;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DefineCircleTest {
 
+    DefineCircle defineCircle = new DefineCircle();
+    DinamicLinkedList.Node first = new DinamicLinkedList.Node(1);
+    DinamicLinkedList.Node two = new DinamicLinkedList.Node(2);
+    DinamicLinkedList.Node third = new DinamicLinkedList.Node(3);
+    DinamicLinkedList.Node four = new DinamicLinkedList.Node(4);
+    @Before
+    public void loadSomeCircleAndNonCircleList() {
+
+        first.next = two;
+        two.next = third;
+        third.next = four;
+    }
+    @Test
+    public void hasCycleReturnYes() {
+        four.next = first;
+        assertTrue(defineCircle.hasCycle(first));
+    }
+    @Test
+    public void hasCycleReturnNo() {
+        four.next = null;
+        assertFalse(defineCircle.hasCycle(first));
+    }
 }
