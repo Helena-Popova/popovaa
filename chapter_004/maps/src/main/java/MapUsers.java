40d5150f7ccdf35c1948fec4e93adaf6219e78ff
Helena-Popova <helezpopova@gmail.com>
2018-06-28 00:40:29 +0300

    hash code

--- chapter_004/maps/src/main/java/MapUsers.java
+++ chapter_004/maps/src/main/java/MapUsers.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapUsers {
+
+    public Map<User, Object> addUsers(User ... users) {
+        Map<User, Object> map = new HashMap<>();
+        Arrays.stream(users).forEach(user -> map.put(user, new Object()));
+        return map;
+    }
 }
--- chapter_004/maps/src/test/java/MapUsersTest.java
+++ chapter_004/maps/src/test/java/MapUsersTest.java
@@ -1,5 +1,23 @@
+import org.json.simple.JSONObject;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class MapUsersTest {
 
+    MapUsers mapUsers = new MapUsers();
+    @Test
+    public void check() {
+        Calendar calendar = new GregorianCalendar(1975,
+                Calendar.DECEMBER, 31);
+        System.out.println(new JSONObject(mapUsers.addUsers(
+                new User("Alex", 3, calendar),
+                new User("Alex", 3, calendar)))
+                .toString());
+    }
 }
