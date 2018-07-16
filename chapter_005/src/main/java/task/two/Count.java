93042520f4253ccdf09492d87c1757dcc25040ed
Helena-Popova <helezpopova@gmail.com>
2018-07-16 06:39:46 +0300

    count

--- chapter_005/src/main/java/task/two/Count.java
+++ chapter_005/src/main/java/task/two/Count.java
@@ -1,4 +1,17 @@
 package task.two;
 
+import lombok.Getter;
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
 public class Count {
+    @GuardedBy("this")
+    @Getter
+    private int value;
+
+    public synchronized void increment() {
+        this.value++;
+    }
+
 }
