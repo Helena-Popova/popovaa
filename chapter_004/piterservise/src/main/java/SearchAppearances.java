639ffbfdea25026870fcb1ff008f50544687f3cf
Helena-Popova <helezpopova@gmail.com>
2018-07-16 23:11:40 +0300

    search word in the file

--- chapter_004/piterservise/src/main/java/SearchAppearances.java
+++ chapter_004/piterservise/src/main/java/SearchAppearances.java
@@ -1,4 +1,78 @@
-package PACKAGE_NAME;
+import lombok.Data;
 
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Stream;
+import java.nio.CharBuffer;
+
+@Data
 public class SearchAppearances {
+
+    HashMap<String, Set<Integer>> appearanes = new HashMap<>();
+
+    /**
+     * Загрузка данных из файла и построение индекса.
+     *
+     * @param filename
+     */
+    public void loadFile(String filename) {
+        List<Character> world = new ArrayList<>();
+        try (FileReader reader = new FileReader(filename)) {
+            char[] buf = new char[256];
+            int c;
+            while ((c = reader.read(buf)) > 0) {
+                if (c < 256) {
+                    buf = Arrays.copyOf(buf, c);
+                }
+
+            }
+            reader.close();
+            toMap(buf);
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private void toMap(char[] buf) {
+        /**
+         * место слова в файле
+         */
+        List<Integer> count = Arrays.asList(0);
+
+        /**
+         * билдер для слова
+         */
+        StringBuilder stringBuilder = new StringBuilder();
+
+        /**
+         * проходимся по буферу, состаляем слово, как только доходим до пробела,
+         * то записываем слово в мапу
+         */
+        CharBuffer.wrap(buf).chars().forEach(alfa -> {
+            if (alfa == ' ' && stringBuilder.length() != 0 || count.get(0) == buf.length - 1) {
+                String word = stringBuilder.toString().toLowerCase();
+                if (!appearanes.containsKey(word)) {
+                    appearanes.put(word, new HashSet<>());
+                }
+                appearanes.get(word).add(count.get(0) - word.length());
+                stringBuilder.setLength(0);
+            } else {
+                stringBuilder.append((char) alfa);
+            }
+            count.set(0, count.get(0) + 1);
+        });
+    }
+
+    /**
+     * Возвращает список позиций слова в файле. Если данного слова в файле нет, то возвращается null.
+     *
+     * @param searchWord
+     * @return
+     */
+    public Set<Integer> getIndexes4Word(String searchWord) {
+        return appearanes.containsKey(searchWord.toLowerCase()) ? appearanes.get(searchWord.toLowerCase()) : null;
+    }
 }
--- chapter_004/piterservise/src/test/java/SearchAppearancesTest.java
+++ chapter_004/piterservise/src/test/java/SearchAppearancesTest.java
@@ -1,5 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class SearchAppearancesTest {
+    SearchAppearances searchAppearances = new SearchAppearances();
+    String filename = "notes3.txt";
+    String text = "Hwo is Jon Gold ? Jon is mysterious engineer";
 
+    @Before
+    public void writeToFileTestText() {
+        try (FileWriter writer = new FileWriter(filename)) {
+            // запись всей строки
+            writer.write(text);
+            writer.flush();
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void countNumbersWord() {
+        searchAppearances.loadFile(filename);
+        assertTrue(searchAppearances.getIndexes4Word("Hwo").contains(text.indexOf("Hwo")));
+        assertTrue(searchAppearances.getIndexes4Word("is").contains(text.indexOf("is")));
+        assertTrue(searchAppearances.getIndexes4Word("jon").contains(text.indexOf("Jon")));
+        assertTrue(searchAppearances.getIndexes4Word("jon").contains(text.indexOf("Jon", 10)));
+        assertTrue(searchAppearances.getIndexes4Word("gOld").contains(text.indexOf("Gold")));
+        assertThat(null, is(searchAppearances.getIndexes4Word("gOl")));
+    }
 }
