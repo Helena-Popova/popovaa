23fe2a41ba25dcedfec39ecb4db06c27ccc18b20
Helena-Popova <helezpopova@gmail.com>
2018-07-16 23:13:09 +0300

    1. Реализовать шаблон Producer Consumer.

--- chapterthreads/whait.chapter/src/main/java/prconsumer/BoundedBlockingQueue.java
+++ chapterthreads/whait.chapter/src/main/java/prconsumer/BoundedBlockingQueue.java
@@ -1,4 +1,76 @@
 package prconsumer;
 
-public class BoundedBlockingQueue {
+import lombok.Getter;
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Для этого вам необходимо реализовать собственную версию bounded blocking queue.
+ * Это блокирующая очередь, ограниченная по размеру.
+ * В данном шаблоне Producer помещает данные в очередь,
+ * а Consumer извлекает данные из очереди.
+ * Если очередь заполнена полностью,
+ * то при попытке добавления поток Producer блокируется,
+ * до тех пор пока Consumer не извлечет очередные данные,
+ * т.е. в очереди появится свободное место.
+ * И наоборот если очередь пуста поток Consumer блокируется,
+ * до тех пор пока Producer не поместит в очередь данные.
+ * В задании нельзя использовать потокобезопасные коллекции реализованные в JDK.
+ * Ваша задача используя, wait/notify реализовать блокирующую очередь.
+ * @author Helen
+ * @param <T>
+ */
+@ThreadSafe
+public class BoundedBlockingQueue<T> {
+
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+
+    @GuardedBy("this")
+    @Getter
+    private int size;
+
+    public BoundedBlockingQueue(int sizeUser) {
+        this.size = sizeUser;
+    }
+
+    public void offer(T value) throws InterruptedException {
+        synchronized (queue) {
+            while (this.queue.size() == this.size) {
+                this.queue.wait();
+            }
+
+            this.queue.offer(value);
+            this.queue.notify();
+        }
+    }
+
+    public void offer(T value, long timeout) throws InterruptedException {
+        synchronized (queue) {
+            if (this.queue.size() == this.size) {
+                this.queue.wait(timeout);
+            }
+            this.queue.offer(value);
+            this.queue.notify();
+        }
+    }
+
+    public T poll() throws InterruptedException {
+        T result;
+        synchronized (queue) {
+            while (this.queue.size() == 0) {
+                this.queue.wait();
+            }
+            result = queue.poll();
+            this.queue.notify();
+        }
+        return result;
+    }
+
+    public int getRealSize() {
+        return this.queue.size();
+    }
 }
--- chapterthreads/whait.chapter/src/test/java/prconsumer/BoundedBlockingQueueTest.java
+++ chapterthreads/whait.chapter/src/test/java/prconsumer/BoundedBlockingQueueTest.java
@@ -1,5 +1,65 @@
+package prconsumer;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
+/**
+ *  В тестах должны быть две нити. одна продюсер, другая пользователь.
+ *
+ * Через методы join добавиться последовательного выполнение программы.
+ *
+ *
+ */
 public class BoundedBlockingQueueTest {
+    BoundedBlockingQueue<Integer> blockingQueue = new BoundedBlockingQueue<>(12);
+    private Thread producer;
+    private Thread consumer;
+
+    @Before
+    public void loadBefore() throws InterruptedException {
+        producer = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int i = 0;
+                    while (i < 10) {
+                        blockingQueue.offer(i);
+                        i++;
+                    }
+                } catch (InterruptedException ie) {
+                    ie.printStackTrace();
+                }
+            }
+        };
+
+        consumer = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int i = 0;
+                    while (i < 10) {
+                        assertThat(blockingQueue.getRealSize(), is(10 - i));
+                        assertThat(blockingQueue.poll(), is(i));
+                        assertThat(blockingQueue.getRealSize(), is(9 - i));
+                        i++;
+                    }
+                } catch (InterruptedException ie) {
+                    ie.printStackTrace();
+                }
+            }
+        };
+    }
+
+    @Test
+    public void producerANDconsumerTest() throws InterruptedException {
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+    }
+
 
 }
