cea87e953bc95acde3941ea7395e14be39627e70
Helena-Popova <helezpopova@gmail.com>
2018-07-24 22:51:24 +0300

    lock

--- chapterthreads/lock.example/src/main/java/Lock.java
+++ chapterthreads/lock.example/src/main/java/Lock.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
 
+@ThreadSafe
 public class Lock {
+    @GuardedBy("this")
+    private boolean isLocked = false;
+
+    /**
+     * проверяет свободен ли лок? Если да - захватывает, иначе блокируется.
+     */
+    public synchronized void lock() throws InterruptedException {
+        while (isLocked) {
+            wait();
+        }
+        this.isLocked = true;
+    }
+
+    /**
+     * проверяет владеет ли поток локом? Если да то - освобождает.
+     */
+    public void unlock() throws InterruptedException {
+        while (!isLocked) {
+            wait();
+        }
+        this.isLocked = false;
+        notifyAll();
+    }
 }
