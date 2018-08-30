d19fff212f27ac2c353b3081e29089fcf0b0c97d
Helena-Popova <helezpopova@gmail.com>
2018-08-30 03:12:12 +0300

    any files

--- chapter_sql/src/main/resources/Magnit.java
+++ chapter_sql/src/main/resources/Magnit.java
@@ -1,18 +1,10 @@
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLReaderFactory;
 import xml.elements.Entry;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.List;
-import java.util.Properties;
 
 
 public class Magnit {
